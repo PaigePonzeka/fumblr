283595d1767dbd8e0c6cfca8727ce9460fb12496
Paige Ponzeka <paigepon@gmail.com>
2011-10-24 22:47:20 -0400

    Code to filter based on tags, setting classes based on tags to be used for filters.

--- public/scripts/theme.js
+++ public/scripts/theme.js
@@ -1,14 +1,23 @@
+var tags = [];
 $(document).ready(function() {
   generateIndex();
 
+  // show the title bar when the user hovers over
   $('.box').live('hover', function(){
     $(this).find('.titlebar').toggleClass('hide');
   });
 
+  // toggle class to increase size of clicked box
   $('.box').live('click', function(){
     $(this).toggleClass('box_large');
     $('#posts_list').isotope( 'reLayout');
     false
+  });
+
+  // filter the boxes when the user clicks a link
+  $("#post_filter a").live('click', function(){
+    console.log($(this).attr('href'));
+    filterIsotope($(this).attr('href'));
   });
 
 });
@@ -31,10 +40,10 @@ function intializeIsotope()
 
   // Sorting elements by tag
   //$('#posts_list').isotope({ sortBy : 'tag' });
-  // Filter posts by tag
-  //$('#posts_list').isotope({ filter: '.photo' });
+
 
 }
+
 
 
 // HELPER - removes images and titles from text and returns the excerpt
@@ -45,7 +54,7 @@ function processText(text)
 }
 
 
-// parses and formats the data given by the tumblr api request (expected format = "2011-09-14 19:59:00 GMT")
+// HELPER - parses and formats the data given by the tumblr api request (expected format = "2011-09-14 19:59:00 GMT")
 function formatDate(date){
   var year_month_day = date.split(" ")[0].split('-');
   month_string = "";
@@ -92,6 +101,15 @@ function formatDate(date){
 }
 
 
+// filter isotope elements
+function filterIsotope(tag_filter) {
+  // remove the #
+  tag_filter = tag_filter.replace('#','.');
+  // Filter posts by tag
+  $('#posts_list').isotope({ filter: tag_filter });
+}
+
+
 // get Index page data and categories
 function generateIndex() {
   var posts_url = "http://api.tumblr.com/v2/blog/hello.ponzeka.com/posts?api_key=4dxnMh8zezG5fq2OK0vwVhusWboTW4SSA4g3JUgJJ55b1QE7Jz&callback&jsonp=?";
@@ -102,14 +120,13 @@ function generateIndex() {
         return;
       // get the post type
       var post_type = this.type;
-      // get post tags (array)
-      var post_tags = this.tags
 
       // get post note count
       var post_note_count = this.note_count
 
       var preview_content;
       // process each post depending on type
+      generateTagsList(this.tags);
       var preview_titlebar = $('<div />', { class: "titlebar" } );
       var preview_titlebar_date = $('<p />', { text: formatDate(this.date), class: "date"});
       var preview_titlebar_note = $('<p />', { text: this.note_count, class: "note"});
@@ -146,7 +163,7 @@ function generateTextPreview(post)
   var text_post_body = $('<p />', { text: processText(post.body).substring(0, 300) });
   var text_post_url = $('<a />', { href: post.post_url});
 
-  preview_container = generatePreviewContainer(post.type);
+  preview_container = generatePreviewContainer(post.tags);
   return preview_container.append(text_post_url.html(text_post_title));
 }
 
@@ -158,7 +175,7 @@ function generatePhotoPreview(post)
   var preview_post_caption = $('<p />', { text: post.caption });
   var preview_post_url = $('<a />', { href: post.post_url });
 
-  post_container = generatePreviewContainer(post.type);
+  post_container = generatePreviewContainer(post.tags);
   return post_container.append(preview_post_photo);
 }
 
@@ -169,7 +186,7 @@ function generateQuotePreview(post)
   var preview_post_caption = $('<h3 />', { text: processText(post.text)});
   var preview_post_url = $('<a />', { href: post.post_url });
 
-  post_container = generatePreviewContainer(post.type);
+  post_container = generatePreviewContainer(post.tags);
   return post_container.append( preview_post_caption);
 }
 
@@ -180,7 +197,7 @@ function generateLinkPreview(post)
   var preview_post_description = $('<h3 />', { text: processText(post.title)});
   var preview_post_url = $('<a />', { href: post.post_url });
 
-  post_container = generatePreviewContainer(post.type);
+  post_container = generatePreviewContainer(post.tags);
   return post_container.append( preview_post_description);
 }
 
@@ -190,9 +207,48 @@ function generatePreviewContainer(type)
 {
   var post = $('<div />', { class: "box" });
   // add class for each tag
-  post.addClass(type);
+  $.each(tags, function(){
+    console.log(tags);
+    post.addClass(this.toLowerCase().replace(/\s+/g,'_'));
+  });
+
   return post;
 }
+
+
+// returns boolean value to determine if the string is in the tags array
+function inTags(tag)
+{
+  var i;
+  tag_compare = tag.toLowerCase();
+  for(i = 0; i <= (tags.length-1); i++)
+  {
+    if (tag_compare == tags[i])
+      {
+        return true;
+      }
+  }
+  return false;
+}
+
+
+// if the tag isn't contained in the tags array then it should be added to the array
+function generateTagsList(post_tags)
+{
+  $.each(post_tags, function(){
+    if (!inTags(this))
+    {
+      tags.push(this.toLowerCase());
+      var tag_nospaces = this.replace(/\s+/g,'_');
+      var tag_list_item = $("<li/>");
+      var tag_list_item_link = $("<a/>", { href: "#" + tag_nospaces}).text("" + this);
+      tag_list_item.html(tag_list_item_link);
+      $('#post_filter').append(tag_list_item);
+    }
+  });
+}
+
+
 
 
 /*******************************************************
